06218533dddfd6845aff453f7beb439877df51f2
Dean L Betty <deanlloyd64@gmail.com>
2019-09-24 16:55:04 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require "pry"
+
+class Artist
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def songs
+    Song.all.select {|x| x.artist == self}
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def add_song_by_name(song_name)
+    song_name = Song.new(song_name)
+    song_name.artist = self
+  end
+  
+  def song_count
+    @@song_count = Song.all.length
+  end
+  
+  #binding.pry
+end
