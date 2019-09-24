ac4689989686a0f651c0b4dc91457f6cfd4491a5
Dean L Betty <deanlloyd64@gmail.com>
2019-09-24 17:37:24 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,8 +2,8 @@ class Post
   attr_accessor :author, :name, :title
   @@all = []
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
   
